2da16dc0cc48157f14552abbdac1fcebf3ecd1f7
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-24 11:39:03 -0300

    substitui múltiplos componentes FoodCard por um componente FoodCatalog e adiciona dados dinâmicos ao catálogo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from './shared/header/header.component';
 import { FoodCardComponent } from './shared/food-card/food-card.component';
+import { FoodCatalogComponent } from './pages/food-catalog/food-catalog.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FoodCardComponent],
+  imports: [RouterOutlet, HeaderComponent, FoodCardComponent, FoodCatalogComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/pages/food-catalog/food-catalog.component.ts
+++ src/app/pages/food-catalog/food-catalog.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Food } from '../../models/food';
 
 @Component({
   selector: 'app-food-catalog',
@@ -8,5 +9,20 @@ import { Component } from '@angular/core';
   styleUrl: './food-catalog.component.scss'
 })
 export class FoodCatalogComponent {
-  
+  foodArray: Food [] = [
+    {
+      title: "Whopper",
+      serves: "1 pessoa",
+      description: `Um hambúrguer com uma carne bovina grelhada de 113g, pão com gergelim, queijo derretido, picles, salada fresca (alface, cebola, tomate), ketchup e maionese bk.`,
+      imageLink:"https://static.ifood-static.com.br/image/upload/t_medium/pratos/6e73dce2-a17f-4aef-9035-1409cea198fe/202408051111_5Q5J_i.jpg",
+      price: 29.99
+    },
+    {
+      title: "Kid jr",
+      serves: "1 pessoa",
+      description: `Um delicioso hambúrguer com carne bovina grelhada de 56g, pão com gergelim, queijo derretido, ketchup e maionese bk e você recebe um brinde surpresa.`,
+      imageLink:"https://static.ifood-static.com.br/image/upload/t_medium/pratos/6e73dce2-a17f-4aef-9035-1409cea198fe/202410091119_A3QE_i.jpg",
+      price: 35.90
+    },
+  ];
 }
