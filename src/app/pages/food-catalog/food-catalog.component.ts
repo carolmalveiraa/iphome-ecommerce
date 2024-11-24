a1407beb7de2c7b234990cf7f6bc4ebb8e6c64e9
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-24 10:37:05 -0300

    adiciona componente FoodCatalog com template e testes

---
+++ src/app/pages/food-catalog/food-catalog.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-food-catalog',
+  standalone: true,
+  imports: [],
+  templateUrl: './food-catalog.component.html',
+  styleUrl: './food-catalog.component.scss'
+})
+export class FoodCatalogComponent {
+
+}
