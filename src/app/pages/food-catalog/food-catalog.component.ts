8f641dfa3170e839be6f0e36e2e7a300cdc1887e
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-24 11:54:03 -0300

    adiciona exibição dinâmica de cards de comida no componente FoodCatalog

--- src/app/pages/food-catalog/food-catalog.component.ts
+++ src/app/pages/food-catalog/food-catalog.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
 import { Food } from '../../models/food';
+import { FoodCardComponent } from "../../shared/food-card/food-card.component";
 
 @Component({
   selector: 'app-food-catalog',
   standalone: true,
-  imports: [],
+  imports: [FoodCardComponent],
   templateUrl: './food-catalog.component.html',
   styleUrl: './food-catalog.component.scss'
 })
@@ -18,7 +19,7 @@ export class FoodCatalogComponent {
       price: 29.99
     },
     {
-      title: "Kid jr",
+      title: "King jr",
       serves: "1 pessoa",
       description: `Um delicioso hambúrguer com carne bovina grelhada de 56g, pão com gergelim, queijo derretido, ketchup e maionese bk e você recebe um brinde surpresa.`,
       imageLink:"https://static.ifood-static.com.br/image/upload/t_medium/pratos/6e73dce2-a17f-4aef-9035-1409cea198fe/202410091119_A3QE_i.jpg",
--- src/app/shared/food-card/food-card.component.ts
+++ src/app/shared/food-card/food-card.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatIconModule } from '@angular/material/icon';
 import { MatCardModule } from '@angular/material/card';
@@ -12,11 +12,5 @@ import { Food } from '../../models/food';
   styleUrl: './food-card.component.scss'
 })
 export class FoodCardComponent {
-  food: Food = {
-    title: "Whopper",
-    serves: "1 pessoa",
-    description: `Um hambúrguer com uma carne bovina grelhada de 113g, pão com gergelim, queijo derretido, picles, salada fresca (alface, cebola, tomate), ketchup e maionese bk.`,
-    imageLink:"https://static.ifood-static.com.br/image/upload/t_medium/pratos/6e73dce2-a17f-4aef-9035-1409cea198fe/202408051111_5Q5J_i.jpg",
-    price: 29.99
-  };
+  @Input() food!: Food;
 }
