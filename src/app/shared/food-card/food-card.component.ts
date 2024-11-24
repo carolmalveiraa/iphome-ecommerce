9d2932e148baaabeee325317e5380bab4c7d860a
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-24 11:29:23 -0300

    atualiza imagem do card de comida e melhora descrição alternativa

--- src/app/shared/food-card/food-card.component.ts
+++ src/app/shared/food-card/food-card.component.ts
@@ -16,7 +16,7 @@ export class FoodCardComponent {
     title: "Whopper",
     serves: "1 pessoa",
     description: `Um hambúrguer com uma carne bovina grelhada de 113g, pão com gergelim, queijo derretido, picles, salada fresca (alface, cebola, tomate), ketchup e maionese bk.`,
-    imageLink: "https://d3sn2rlrwxy0ce.cloudfront.net/Banner-Whopper-principal-m.jpg?mtime=20210416235547&focal=none",
+    imageLink:"https://static.ifood-static.com.br/image/upload/t_medium/pratos/6e73dce2-a17f-4aef-9035-1409cea198fe/202408051111_5Q5J_i.jpg",
     price: 29.99
-  }
+  };
 }
