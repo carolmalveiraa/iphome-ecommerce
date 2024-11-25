3cb8499cc9b4e001afce3e6e8e4fdb7465ca6e45
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-25 18:58:13 -0300

    ajusta formatação do componente de cabeçalho e adiciona importação do MatDrawer

--- src/app/shared/header/header.component.ts
+++ src/app/shared/header/header.component.ts
@@ -1,7 +1,8 @@
-import {Component} from '@angular/core';
+import {Component, OnInit, Input} from '@angular/core';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatIconModule} from '@angular/material/icon';
 import {MatButtonModule} from '@angular/material/button';
+import {MatDrawer} from '@angular/material/sidenav';
 
 @Component({
   selector: 'app-header',
