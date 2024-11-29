722b9b766d1fce1371b00d20c4f969fde33b4e54
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-29 17:53:47 -0300

    atualiza componente de carrinho para exibir itens com botões de adicionar e remover, e ajusta estilo para melhor apresentação

--- src/app/shared/cart/cart.component.ts
+++ src/app/shared/cart/cart.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 import { MatListModule } from '@angular/material/list';
 import { MatDividerModule } from '@angular/material/divider';
+import { MatButtonModule } from '@angular/material/button';
+import { MatIconModule } from '@angular/material/icon';
+
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [MatListModule, MatDividerModule],
+  imports: [MatListModule, MatDividerModule, MatButtonModule, MatIconModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
