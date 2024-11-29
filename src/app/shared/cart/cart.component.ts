3e35891a9588779f1e8ef2d1fef1a472d7075cd6
Carol Malveira <carolmalveiraa12@icloud.com>
2024-11-29 17:35:09 -0300

    atualiza componente de carrinho para exibir lista de itens e remove testes associados

--- src/app/shared/cart/cart.component.ts
+++ src/app/shared/cart/cart.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
-
+import { MatListModule } from '@angular/material/list';
+import { MatDividerModule } from '@angular/material/divider';
 @Component({
   selector: 'app-cart',
   standalone: true,
-  imports: [],
+  imports: [MatListModule, MatDividerModule],
   templateUrl: './cart.component.html',
   styleUrl: './cart.component.scss'
 })
