9758430e7ece9dfa38a77e9dc69a64483e94795f
Carol Malveira <carolmalveiraa12@icloud.com>
2024-12-01 14:53:20 -0300

    atualiza estrutura de rotas para incluir componentes de catálogo e formulário de comida, ajusta layout do componente principal

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { FoodFormComponent } from './pages/food-form/food-form.component';
+import { FoodCatalogComponent } from './pages/food-catalog/food-catalog.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: "", component: FoodCatalogComponent },
+  { path: "form", component: FoodFormComponent },
+];
