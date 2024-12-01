fe1e6de3082e7ac2e20d233387607f9c05322d50
Carol Malveira <carolmalveiraa12@icloud.com>
2024-12-01 15:15:45 -0300

    adiciona formulário de comida com validações e estilos, atualiza o componente para incluir módulos do Angular Material

--- src/app/pages/food-form/food-form.component.ts
+++ src/app/pages/food-form/food-form.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatButtonModule } from '@angular/material/button';
 
 @Component({
   selector: 'app-food-form',
   standalone: true,
-  imports: [],
+  imports: [MatInputModule, MatFormFieldModule, MatButtonModule],
   templateUrl: './food-form.component.html',
   styleUrl: './food-form.component.scss'
 })
