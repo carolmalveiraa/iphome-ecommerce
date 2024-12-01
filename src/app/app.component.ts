ad2bde9627b7d2e3d6af76ffa97556f603996465
Carol Malveira <carolmalveiraa12@icloud.com>
2024-12-01 14:37:22 -0300

    adiciona componente de formulário de comida e atualiza o componente principal para utilizá-lo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,10 +6,11 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FoodCatalogComponent } from './pages/food-catalog/food-catalog.component';
 import { CartComponent } from './shared/cart/cart.component';
 import { CartService } from './services/cart.service';
+import { FoodFormComponent } from './pages/food-form/food-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FoodCatalogComponent, MatSidenavModule, CartComponent],
+  imports: [RouterOutlet, HeaderComponent, FoodCatalogComponent, MatSidenavModule, CartComponent, FoodFormComponent],
   providers: [CartService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
