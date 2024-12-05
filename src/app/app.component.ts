f15f56b307694653c4a5b11dc1ff4ae2cd12b250
Carol Malveira <carolmalveiraa12@icloud.com>
2024-12-04 23:42:11 -0300

    remove FoodFormComponent do app.component.ts; ajusta imports para simplificação do módulo

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,11 +6,10 @@ import { HeaderComponent } from './shared/header/header.component';
 import { FoodCatalogComponent } from './pages/food-catalog/food-catalog.component';
 import { CartComponent } from './shared/cart/cart.component';
 import { CartService } from './services/cart.service';
-import { FoodFormComponent } from './pages/food-form/food-form.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, FoodCatalogComponent, MatSidenavModule, CartComponent, FoodFormComponent],
+  imports: [RouterOutlet, HeaderComponent, MatSidenavModule, CartComponent],
   providers: [CartService],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
